61cfa311096b3c444f43868a6574c3c5e8a744c5
kemuritechnology <girish@kemuri.in>
2023-02-28 12:32:44 +0530

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr: false,
-    target: 'static',
-    // srcDir: 'src',
-    modules: ['@nuxtjs/tailwindcss'],
-    // generate: {
-    //     dir: 'my-site'
-    // }
+    srcDir: 'src',
+    modules: ['@nuxtjs/tailwindcss']
 })
